dbfbeb29d42a7fe6e3dfce0a0c5154d74b7a6841
scootero <61133217+scootero@users.noreply.github.com>
2022-11-14 21:05:14 -0600

    site done nav done footer reworked colors fixed allll gooood

--- REID/script.js
+++ REID/script.js
@@ -1,6 +1,25 @@
 let path = document.querySelector("#house-path");
 
-let length = path.getTotalLength;
-// alert('test')
-path.style.strokeDasharray = length + " " + length;
-path.style.strokeDashoffset = length;
+// let length = path.getTotalLength;
+// // alert('test')
+// path.style.strokeDasharray = length + " " + length;
+// path.style.strokeDashoffset = length;
+
+const hamburger = document.getElementById("hamburger");
+const boxGroup1 = document.getElementsByClassName("boxes-group-1");
+const boxGroup2 = document.getElementsByClassName("boxes-group-2");
+const boxText = document.getElementsByClassName("box-text");
+hamburger.addEventListener("click", () => {
+  for (i = 0; i < 4; i++) {
+    boxGroup1[i].classList.toggle("boxes-slide-left");
+  }
+
+  for (i = 0; i < 3; i++) {
+    boxText[i].classList.toggle("opacity-0");
+    boxText[i].classList.toggle("opacity-1");
+  }
+  for (i = 0; i <= 4; i++) {
+    boxGroup2[i].classList.toggle(`box-${i + 4}-down`);
+    boxGroup2[i].classList.toggle("boxes-slide-up");
+  }
+});
